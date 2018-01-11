3282607f48edd850a28677a0d1d81448236ca102
Anna Khamsamran <akamsamran1@cnm.edu>
2018-01-10 18:58:52 -0700

    moved entity tables above persona stuff, since we are required to complete this part of the assignment first

--- index.php
+++ index.php
@@ -19,53 +19,6 @@
 		<main>
 			<h1>Data Design</h1>
 			<div>
-				<h2>Personas, Use Cases, & Interaction Flow</h2>
-				<h3>Persona:</h3>
-				<ul>
-					<li><strong>Name</strong>: </li>
-					<li><strong>Personality</strong>:</li>
-					<li><strong>Gender</strong>:</li>
-					<li><strong>Age</strong>:</li>
-					<li><strong>Technology</strong>:</li>
-					<ul>
-						<li><strong>Device</strong>Device:</li>
-						<li><strong>Proficiency</strong>Proficiency:</li>
-						<li><strong>Love/Hate</strong>Love/hate:</li>
-					</ul>
-					<li><strong>Attitudes & Needs</strong>Attitudes & needs:</li>
-					<ul>
-						<li><strong>What need does this person have?</strong></li>
-						<li><strong>Why choose your site over other options?</strong></li>
-					</ul>
-				</ul>
-
-				<h3>User Story</h3>
-				<h4><em>"As a ___(role as relative to the application)___user, I want to__(what do they need to do)__</em>"</h4>
-				<p></p>
-				<h3>Use Case</h3>
-				<ul>
-					<li><strong>Title</strong></li>
-					<li><strong>Name of the "actor, user or Persona, and their role</strong></li>
-					<li><strong>Usage Preconditions</strong></li>
-					<li><strong>Usage Postconditions</strong></li>
-					<li><strong>Interactrion Flow(s), including alternates, if appropriate</strong></li>
-					<li><strong>Frequency of Use</strong></li>
-				</ul>
-				<h3>Interaction Flow:</h3>
-				<ul>
-					<li><strong>User Action 1</strong></li>
-					<li><strong>System Response 1</strong></li>
-					<li><strong>User Action 2</strong></li>
-					<li><strong>System Response 2</strong></li>
-					<li><strong>User Action 3</strong></li>
-					<li><strong>System Response 3</strong></li>
-					<li><strong>User Action 4</strong></li>
-					<li><strong>System Response 4</strong></li>
-					<li><strong>User Action 5</strong></li>
-					<li><strong>System Response 5</strong></li>
-				</ul>
-			</div>
-			<div>
 				<h2>Entities & Attributes</h2>
 				<p></p>
 				<table>
@@ -250,8 +203,55 @@
 						<td>TD 10 3</td>
 					</tr>
 				</table>
-
 			</div>
+			<div>
+				<h2>Personas, Use Cases, & Interaction Flow</h2>
+				<h3>Persona:</h3>
+				<ul>
+					<li><strong>Name</strong>: </li>
+					<li><strong>Personality</strong>:</li>
+					<li><strong>Gender</strong>:</li>
+					<li><strong>Age</strong>:</li>
+					<li><strong>Technology</strong>:</li>
+					<ul>
+						<li><strong>Device</strong>Device:</li>
+						<li><strong>Proficiency</strong>Proficiency:</li>
+						<li><strong>Love/Hate</strong>Love/hate:</li>
+					</ul>
+					<li><strong>Attitudes & Needs</strong>Attitudes & needs:</li>
+					<ul>
+						<li><strong>What need does this person have?</strong></li>
+						<li><strong>Why choose your site over other options?</strong></li>
+					</ul>
+				</ul>
+
+				<h3>User Story</h3>
+				<h4><em>"As a ___(role as relative to the application)___user, I want to__(what do they need to do)__</em>"</h4>
+				<p></p>
+				<h3>Use Case</h3>
+				<ul>
+					<li><strong>Title</strong></li>
+					<li><strong>Name of the "actor, user or Persona, and their role</strong></li>
+					<li><strong>Usage Preconditions</strong></li>
+					<li><strong>Usage Postconditions</strong></li>
+					<li><strong>Interactrion Flow(s), including alternates, if appropriate</strong></li>
+					<li><strong>Frequency of Use</strong></li>
+				</ul>
+				<h3>Interaction Flow:</h3>
+				<ul>
+					<li><strong>User Action 1</strong></li>
+					<li><strong>System Response 1</strong></li>
+					<li><strong>User Action 2</strong></li>
+					<li><strong>System Response 2</strong></li>
+					<li><strong>User Action 3</strong></li>
+					<li><strong>System Response 3</strong></li>
+					<li><strong>User Action 4</strong></li>
+					<li><strong>System Response 4</strong></li>
+					<li><strong>User Action 5</strong></li>
+					<li><strong>System Response 5</strong></li>
+				</ul>
+			</div>
+
 		</main>
 
 	</body>
