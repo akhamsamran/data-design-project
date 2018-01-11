8a3df6802bb6b431fbcc6f6718897a62566e72ad
Anna Khamsamran <akamsamran1@cnm.edu>
2018-01-10 18:16:01 -0700

    created basic index.php file with a structure

---
+++ index.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anna
+ * Date: 1/10/2018
+ * Time: 3:27 PM
+ */
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8"/>
+		<title>Title</title>
+	</head>
+	<body>
+		<main>
+			<h1></h1>
+			<div>
+				<h2></h2>
+				<h3></h3>
+				<ul>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+				</ul>
+
+				<h3></h3>
+				<ul>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+				</ul>
+			</div>
+			<div>
+				<h2></h2>
+				<p></p>
+				<table>
+					<tr>
+						<th>TH 1</th>
+						<th>TH 2</th>
+						<th>TH 3</th>
+						<th>TH 4</th>
+					</tr>
+					<tr>
+						<th>TD 1 1</th>
+						<td>TD 1 2</td>
+						<td>TD 1 3</td>
+						<td>TD 1 4</td>
+					</tr>
+					<tr>
+						<th>TD 2 1</th>
+						<td>TD 2 2</td>
+						<td>TD 2 3</td>
+						<td>TD 2 4</td>
+					</tr>
+					<tr>
+						<th>TD 3 1</th>
+						<td>TD 3 2</td>
+						<td>TD 3 3</td>
+						<td>TD 3 4</td>
+					</tr>
+					<tr>
+						<th>TD 4 1</th>
+						<td>TD 4 2</td>
+						<td>TD 4 3</td>
+						<td>TD 4 4</td>
+					</tr>
+				</table>
+
+			</div>
+		</main>
+
+	</body>
+</html>
