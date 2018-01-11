3939ecd61047bc5b0153e6edcbee9e4e240fc2be
Anna Khamsamran <akamsamran1@cnm.edu>
2018-01-10 20:39:11 -0700

    enter information into table 1 for 1st entity: blogger/user

--- index.php
+++ index.php
@@ -15,46 +15,46 @@
 			<div>
 				<h2>Entities & Attributes</h2>
 				<!--the <p> below is to enter information about what website/app this is about, or notes on the project-->
-				<p></p>
+				<p>Here is a first-attempt to grok <em>Medium</em> blog website</p>
 				<!--the entity tables below each have space for up to 10 attributes. In the case that there are less, rows can be deleted, but no more should be added to a table, as each entity should have not more than 10 attributes.  Copy and paste a whole table when more are needed, delete the ones that are not needed.-->
 				<table>
 					<thead>
-						<th colspan="3"><strong>Entity Name:</strong>camelCase </th>
+						<th colspan="3"><strong>Entity Name:</strong>blogger</th>
 					</thead>
 					<tr>
-						<th>attributeName (entityAttribute:)</th>
+						<th>attributeName:(entityAttribute)</th>
 						<th>PrimaryKey:</th>
 						<th>Note:</th>
 					</tr>
 					<tr>
-						<th>TD 1 1</th>
-						<td>TD 1 2</td>
-						<td>TD 1 3</td>
+						<th>bloggerUserName(ID)</th>
+						<td>Primary Key</td>
+						<td>profileId (primary key)</td>
 					</tr>
 					<tr>
-						<th>TD 2 1</th>
-						<td>TD 2 2</td>
-						<td>TD 2 3</td>
+						<th>bloggerFirstName</th>
+						<td></td>
+						<td></td>
 					</tr>
 					<tr>
-						<th>TD 3 1</th>
-						<td>TD 3 2</td>
-						<td>TD 3 3</td>
+						<th>bloggerLastname</th>
+						<td></td>
+						<td></td>
 					</tr>
 					<tr>
-						<th>TD 4 1</th>
-						<td>TD 4 2</td>
-						<td>TD 4 3</td>
+						<th>bloggerEmail</th>
+						<td></td>
+						<td></td>
 					</tr>
 					<tr>
-						<th>TD 5 1</th>
-						<td>TD 5 2</td>
-						<td>TD 5 3</td>
+						<th>bloggerPassword</th>
+						<td></td>
+						<td>profileHash</td>
 					</tr>
 					<tr>
-						<th>TD 6 1</th>
-						<td>TD 6 2</td>
-						<td>TD 6 3</td>
+						<th>bloggerProfile</th>
+						<td></td>
+						<td>about the blogger/short bio</td>
 					</tr>
 					<tr>
 						<th>TD 7 1</th>
